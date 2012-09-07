560f1cf7b53dc35979781ff24ab001c347cd3d79
Richard Spiers <richard@thoughtexpress.com>
2012-09-07 14:53:39 +0200

    Skeleton SIP AS framework

---
+++ VoiceMailServer/VoiceMailServer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using SIPLib.SIP;
+using SIPLib.Utils;
+
+namespace VoiceMailServer
+{
+    class Program
+    {
+        public static SIPStack CreateStack(SIPApp app, string proxyIp = null, int proxyPort = -1)
+        {
+            SIPStack myStack = new SIPStack(app) { Uri = { User = "alice" } };
+            if (proxyIp != null)
+            {
+                myStack.ProxyHost = proxyIp;
+                myStack.ProxyPort = (proxyPort == -1) ? 5060 : proxyPort;
+            }
+            return myStack;
+        }
+
+        public static TransportInfo CreateTransport(string listenIp, int listenPort)
+        {
+            return new TransportInfo(IPAddress.Parse(listenIp), listenPort, System.Net.Sockets.ProtocolType.Udp);
+        }
+
+        static void Main(string[] args)
+        {
+            TransportInfo localTransport = CreateTransport(Helpers.GetLocalIP(), 5060);
+            SIPApp app = new SIPApp(localTransport);
+            const string pcscfIP = "192.168.0.7";
+            const int pcscfPort = 4060;
+
+            SIPStack stack = CreateStack(app, pcscfIP, pcscfPort);
+            Console.ReadKey();
+        }
+    }
+}
