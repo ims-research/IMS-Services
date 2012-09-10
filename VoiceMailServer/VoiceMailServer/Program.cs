4020f33e258f65f5dd59e97c583c3542767af792
Richard Spiers <richard@thoughtexpress.com>
2012-09-10 21:14:37 +0200

    Minor fix to stack creation
    Also accepts BYE and cancel requests

--- VoiceMailServer/VoiceMailServer/Program.cs
+++ VoiceMailServer/VoiceMailServer/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Text;
 using SIPLib.SIP;
 using SIPLib.Utils;
 using log4net;
@@ -12,9 +9,10 @@ namespace VoiceMailServer
     class Program
     {
         private static readonly ILog Log = LogManager.GetLogger(typeof(SIPApp));
+        private static SIPApp _app;
         public static SIPStack CreateStack(SIPApp app, string proxyIp = null, int proxyPort = -1)
         {
-            SIPStack myStack = new SIPStack(app) { Uri = { User = "alice" } };
+            SIPStack myStack = new SIPStack(app);
             if (proxyIp != null)
             {
                 myStack.ProxyHost = proxyIp;
@@ -31,22 +29,74 @@ namespace VoiceMailServer
         static void AppResponseRecvEvent(object sender, SipMessageEventArgs e)
         {
             Log.Info("Response Received:"+e.Message);
+            Message response = e.Message;
+            string requestType = response.First("CSeq").ToString().Trim().Split()[1].ToUpper();
+            switch (requestType)
+            {
+                case "INVITE":
+                case "REGISTER":
+                case "BYE":
+                default:
+                    Log.Info("Response for Request Type " + requestType + " is unhandled ");
+                    break;
+            }
         }
 
         static void AppRequestRecvEvent(object sender, SipMessageEventArgs e)
         {
             Log.Info("Request Received:" + e.Message);
+            Message request = e.Message;
+            switch (request.Method.ToUpper())
+            {
+                case "INVITE":
+                    {
+                        
+                        
+                        _app.Useragents.Add(e.UA);
+                        Message m = e.UA.CreateResponse(200, "OK");
+                        e.UA.SendResponse(m);
+                        break;
+                    }
+                case "BYE":
+                    {
+                        _app.Useragents.Add(e.UA);
+                        Message m = e.UA.CreateResponse(200, "OK");
+                        e.UA.SendResponse(m);
+                        break;
+                    }
+                case "CANCEL":
+                    {
+                        _app.Useragents.Add(e.UA);
+                        Message m = e.UA.CreateResponse(200, "OK");
+                        e.UA.SendResponse(m);
+                        break;
+                    }
+                case "ACK":
+                case "MESSAGE":
+                case "OPTIONS":
+                case "REFER":
+                case "SUBSCRIBE":
+                case "NOTIFY":
+                case "PUBLISH":
+                case "INFO":
+                default:
+                    {
+                        Log.Info("Request with method " + request.Method.ToUpper() + " is unhandled");
+                        break;
+                    }
+            }
         }
 
         static void Main(string[] args)
         {
             TransportInfo localTransport = CreateTransport(Helpers.GetLocalIP(), 7000);
-            SIPApp app = new SIPApp(localTransport);
-            app.RequestRecvEvent += new EventHandler<SipMessageEventArgs>(AppRequestRecvEvent);
-            app.ResponseRecvEvent += new EventHandler<SipMessageEventArgs>(AppResponseRecvEvent);
+            _app = new SIPApp(localTransport);
+            _app.RequestRecvEvent += new EventHandler<SipMessageEventArgs>(AppRequestRecvEvent);
+            _app.ResponseRecvEvent += new EventHandler<SipMessageEventArgs>(AppResponseRecvEvent);
             const string scscfIP = "192.168.20.248";
             const int scscfPort = 4060;
-            SIPStack stack = CreateStack(app, scscfIP, scscfPort);
+            SIPStack stack = CreateStack(_app, scscfIP, scscfPort);
+            stack.Uri = new SIPURI("voicemail@open-ims.test");
             Console.ReadKey();
         }
     }
